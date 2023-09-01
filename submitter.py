c1ad1ec7c2abd9dab74582d02c95e8b1dc0c104f
a13xie@Sodium <rostik.medved@gmail.com>
2023-09-01 22:37:50 +0200

    smt

--- submitter.py
+++ submitter.py
@@ -1,5 +1,18 @@
+import bpy
+import json
+
 import sys
 argv = sys.argv
 argv = argv[argv.index("--") + 1:]  # get all args after "--"
 
-print(argv)
+bpy.ops.flamenco.fetch_job_types()
+
+job_types_json = json.loads(bpy.data.scenes["Scene"].flamenco_available_job_types_json)
+job_types = []
+for entry in job_types_json["job_types"]:
+    job_types.append(entry["name"])
+
+if argv[0] is in job_types:
+    print("Nice, I found this job type!")
+
+bpy.data.scenes["Scene"].flamenco_job_type
