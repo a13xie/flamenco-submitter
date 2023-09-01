ac89023713cbe7bff70f2563ed4d5bf4c4f6e774
a13xie@Sodium <rostik.medved@gmail.com>
2023-09-01 21:30:33 +0200

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Usage: ./setup.sh FLAMENCO_URL
+
+set -xe
+
+# Download the Flamenco addon
+wget $1/flamenco3-addon.zip -O /tmp/flamenco.zip
+
+# Launch Blender with the install script
+/home/a13xie/apps/blender-3.6.2/blender -b -y --python-exit-code 1 --python-console << EOF
+import bpy
+bpy.ops.preferences.addon_install(filepath='/tmp/flamenco.zip')
+bpy.ops.preferences.addon_enable(module='flamenco')
+bpy.context.preferences.addons['flamenco'].preferences['manager_url'] = "$1"
+bpy.ops.flamenco.ping_manager()
+bpy.ops.wm.save_userpref()
+
+EOF
+
+set -e
+echo "Addon installed, enabled and configured!"
