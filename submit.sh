3457d5cbdb808bb9db6e20e7f61914c3ceccd4f2
a13xie@Sodium <rostik.medved@gmail.com>
2023-09-02 09:57:31 +0200

    Initial commit for the submit script

---
+++ submit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Detect version control system
+# Get a list of changed files, iterate through them and submit each to Flamenco
+# Needs logic for killing off Blender instances since Blender doesn't quit after submission
